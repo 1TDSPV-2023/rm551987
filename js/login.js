f08cecffac08cb4ee0f7b3b6c1c908f901271783
Edurb23 <rm551987@fiap.com.br>
2023-05-16 22:58:36 -0300

    login js

--- js/login.js
+++ js/login.js
@@ -1,35 +1,89 @@
 "use strict";
 
-const usuario1 = { 
-    nomeUsuario : "rita-lee", 
-    senhaUsuario: "98762"
-}
+//MOSTRAR SENHA
 
-const usuario2 = { 
-    nomeUsuario : "pt01", 
-    senhaUsuario: "7777"
-}
 
-console.log()
-usuario2.nomeUsuario = "Juquinha";
-console.log(usuario2.nomeUsuario);
+
+//MOSTRAR SENHA
+
+//CRIANDO OBJETOS
+
 
 //Lista de usuario
 
-let listDeusuario = [];
-listDeusuario.push(usuario1);
-listDeusuario.push(usuario2);
+let listDeusuario = [
+
+    {
+        nomeCompleto : "José das Couves",
+        nomeUsuario : "Jose",
+        senhaUsuario : 123456
+    }
+
+    ,
+
+    
+    {
+        nomeCompleto : "João Paulino",
+        nomeUsuario : "Joao",
+        senhaUsuario : 123456
+    }
+
+    ,
+
+    {
+        nomeCompleto : "Maria Tomaite",
+        nomeUsuario : "Maria",
+        senhaUsuario : 123456
+    }
+    ,
+    {
+        nomeCompleto : "Paulo da Selva",
+        nomeUsuario : "paulo",
+        senhaUsuario : 123456
+    }
+
+
+
+];
+
+localStorage.setItem( 'listaUser', JSON.stringify(listDeusuario));
+
+
+
 
 addEventListener('click', (evento) => {
     let userInput = document.querySelector("#idUser");
     
     let passwordInput = document.querySelector("#idPass");
     
+   //MOSTRAR SENHA
+   // const eyePass = document.querySelector(".fa-eyes");
+
+   if(evento.target.className == "fa fa-eye" || evento.target.className == "fa fa-eye-slash"){
+
+    evento.target.setAttribute("style","cursor:pointer;")
+
+    if(passInput.getAttribute("type") == "password"){
+        passInput.setAttribute("type","text");
+        evento.target.setAttribute("class","fa fa-eye-slash")  
+    }else{
+        evento.target.setAttribute("style","cursor:pointer");
+        evento.target.setAttribute("class","fa fa-eye");  
+        passInput.setAttribute("type","password"); 
+    }
+
+
+}
+
+
+    let listDeusuarioRecuperada =  JSON.parse(localStorage.getItem("listaUser"))
+    
+    
     if (evento.target.id == "btnSubmit") {
         
        try{
        
-        listDeusuario.forEach( (usuario) => {
+        listDeusuarioRecuperada.forEach( (usuario) => {
 
         
         if (userInput.value == usuario.nomeUsuario && passwordInput.value == usuario.senhaUsuario) {
@@ -45,7 +99,9 @@ addEventListener('click', (evento) => {
     
         }catch(err){
             if(err == "Usuário VALIDO !!!"){
-                console.log("Usuário VALIDO !!!");
+                
+                window.location.href = "../sucesso.html"
+            
             }
             else{
                 console.log("Usuário ou Senha Invalido!!!");
